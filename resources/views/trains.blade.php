c17ed0b6ec91f3541de41a5feafa39aa8fda0946
VincenzoMinardi <vincenzo.minardi.mv@gmail.com>
2023-06-27 18:43:32 +0200

    add: trains with array faker

---
+++ resources/views/trains.blade.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>Laravel-migration-seeder</title>
+        @vite('resources/js/app.js')
+    </head>
+    <body>
+        <h1 class="text-center">TRAINS</h1>
+        <table class="table">
+            <thead>
+              <tr>
+                <th scope="col">#</th>
+                <th scope="col">First</th>
+                <th scope="col">Last</th>
+                <th scope="col">Handle</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <th scope="row">1</th>
+                <td>Mark</td>
+                <td>Otto</td>
+                <td>@mdo</td>
+              </tr>
+              <tr>
+                <th scope="row">2</th>
+                <td>Jacob</td>
+                <td>Thornton</td>
+                <td>@fat</td>
+              </tr>
+              <tr>
+                <th scope="row">3</th>
+                <td colspan="2">Larry the Bird</td>
+                <td>@twitter</td>
+              </tr>
+            </tbody>
+          </table>
+    </body>
+</html>
+
+
+
+
+
+{{-- <img src="{{ Vite::asset('resources/img/picsum30.jpg') }}" alt=""> --}}
